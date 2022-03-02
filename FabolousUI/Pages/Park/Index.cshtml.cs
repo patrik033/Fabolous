c10cab952b1e4253cebf556f68fda5d04923e301
patrik033 <patrik.odh@gmail.com>
2022-03-02 16:12:52 +0100

    Add project files.

---
+++ FabolousUI/Pages/Park/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FabolousUI.Pages.Park
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
