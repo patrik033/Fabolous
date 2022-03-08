712deb17ad6ac126875e02dd7bb08310009843b0
patrik033 <patrik.odh@gmail.com>
2022-03-08 16:30:46 +0100

    commit

--- BussinessLogicLibrary/Stuff/Parkingspot.cs
+++ BussinessLogicLibrary/Stuff/Parkingspot.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BussinessLogicLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,6 @@ namespace BussinessLogicLibrary
     public class Parkingspot
     {
         public int Size { get; set; } = 4;  // 0 = Full 2 = Delvis full 4 = Tom
-        public Dictionary<string, object> Parked_Vehicles { get; set; }
+        public List<Vehicle> Parked_Vehicles { get; set; } = new List<Vehicle>();
     }
 }
